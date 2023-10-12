87c07b28755c49371d802914134b43c87c9f87e4
Itaira <itairassa@gmail.com>
2023-10-12 15:29:15 +0200

    Login first step

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {LoginPayload} from '../login-payload';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  loginPayload: LoginPayload;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.loginPayload.username = this.loginForm.get('username')!.value;
+    this.loginPayload.password = this.loginForm.get('password')!.value;
+  }
 
 }
